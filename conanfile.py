b2588e63f7b3d9c7f53ab4a9113d3ab362714ee2
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2018-09-20 12:28:22 +0300

    Change msvc profile. Fix build script

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,13 @@ class DbusConan(ConanFile):
     version = "1.12.10"
     DBUS_FOLDER_NAME = "dbus-%s" % version
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False" # UNDONE ignore options now
+    options = {"nolink": [True, False]}
+    default_options = "nolink=False"
+    #options = {"shared": [True, False]}
+    #default_options = "shared=False" # UNDONE ignore options now
     description = "D-Bus is a simple system for interprocess communication and coordination."
-    generators = "cmake_find_package"
+    #generators = "cmake_find_package"
+    generators = ["cmake_find_package","cmake"]
     
     def build_requirements(self):
       self.build_requires("expat/2.2.6@common/stable")
@@ -29,10 +32,14 @@ class DbusConan(ConanFile):
       cmake = CMake(self, parallel=True)
       
       # HACK Dbus use different names than conan cmake_find_package-generator and also ignore EXPAT_DEFINITIONS
+      tools.replace_in_file("src/cmake/CMakeLists.txt", "project(dbus)", "project(dbus)\ninclude(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\nconan_basic_setup()")
+
       tools.replace_in_file("Findexpat.cmake", "expat_", "EXPAT_")
       tools.replace_in_file("Findexpat.cmake", "EXPAT_INCLUDE_DIRS", "EXPAT_INCLUDE_DIR")
       tools.replace_in_file("src/cmake/CMakeLists.txt", "find_package(EXPAT)", "find_package(EXPAT)\nadd_definitions(${EXPAT_DEFINITIONS})")
       
+      cmake.definitions["CONAN_CXX_FLAGS"] = "/Zc:threadSafeInit-"
+      cmake.definitions["CONAN_C_FLAGS"] = "/Zc:threadSafeInit-"
       cmake.configure(source_folder="src/cmake")
       cmake.build()
 
@@ -42,18 +49,12 @@ class DbusConan(ConanFile):
       self.copy("*.lib", dst="lib", keep_path=False)
 
     def package_info(self):
-        libs = None
-        if self.settings.os == "Windows":
-            libs = ["dbus"]
-            if self.options.shared:
-                self.cpp_info.defines =  ["DBUS_DLL"]
-            else:
-                libs = [i + "static" for i in libs]
-            if self.settings.build_type == "Debug":
-                libs = [i + "d" for i in libs]
+        libs = []
+        if self.options.nolink == "False":
+          libs = ["dbus-1"]
+          if self.settings.os == "Windows":
             libs = [i + ".lib" for i in libs]
-        else:
-            libs = ["dbus"]
+            
         self.cpp_info.libs = libs
         self.cpp_info.includedirs = ['include', 'include/dbus']  # Ordered list of include paths
         self.cpp_info.libdirs = ['lib']  # Directories where libraries can be found
